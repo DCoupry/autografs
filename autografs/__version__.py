2fe96f9be74749766cb9087785bc49bdf8153293
DCoupry <damien.coupry@gmail.com>
2018-09-04 17:32:57 +0200

    version change, minor

--- autografs/__version__.py
+++ autografs/__version__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-VERSION = (2, 3, 1)
+VERSION = (2, 3, 2)
 __version__ = '.'.join(map(str, VERSION))
--- autografs/framework.py
+++ autografs/framework.py
@@ -6,7 +6,7 @@ __author__  = "Damien Coupry"
 __credits__ = ["Prof. Matthew Addicoat"]
 __license__ = "MIT"
 __maintainer__ = "Damien Coupry"
-__version__ = '2.3.1'
+__version__ = '2.3.2'
 __status__  = "production"
 
 import os
