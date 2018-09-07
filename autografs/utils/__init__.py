ce57ebcfce1c4ab12c42e1f4b043b06bd7c19d1c
DCoupry <damien.coupry@gmail.com>
2018-09-07 12:55:08 +0200

    autografs/utils/operations.py now documented and pep8

--- autografs/utils/__init__.py
+++ autografs/utils/__init__.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*-
 # Copyright : see accompanying license files for details
 
-__author__  = "Damien Coupry"
+__author__ = "Damien Coupry"
 __credits__ = ["Prof. Matthew Addicoat"]
 __license__ = "MIT"
 __maintainer__ = "Damien Coupry"
 __version__ = '2.3.2'
-__status__  = "production"
+__status__ = "production"
 
 
 import os
 
-__all__     = ["topology","sbu","operations","mmanalysis","io","symmetry"]
-__data__    = os.path.join("/".join(os.path.dirname(__file__).split("/")[:-1]),"data")
+__all__ = ["topology", "sbu", "operations", "mmanalysis", "io", "symmetry"]
+__data__ = os.path.join(
+    "/".join(os.path.dirname(__file__).split("/")[:-1]), "data")
